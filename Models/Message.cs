afbd29245917dd7ed0eab69bd1e886b4303843f6
Sethias15 <olivier.s.morin@gmail.com>
2023-05-23 12:52:29 -0400

    Add Modèle message

---
+++ Models/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatManager.Models
+{
+    public class Message
+    {
+        public Message(int senderId, int receiverId, string message)
+        {
+            SenderId = senderId;
+            ReceiverId = receiverId;
+            TextMessage = message;
+        }
+        public Message()
+        {
+            SenderId = -1;
+            ReceiverId = -1;
+            TextMessage = "";
+        }
+        #region Data Members
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public string TextMessage { get; set; }
+        #endregion
+    }
+}
